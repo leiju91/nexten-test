dcbd9777aab5fbf8ee16451064fb6a90804be87e
Julie Lacresse <61269042+leiju91@users.noreply.github.com>
2021-04-14 11:48:29 +0200

    lien du repo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,6 +11,7 @@ const Home = () => {
     return (
         <div className="Home">
             <h1>Test Julie</h1>
+            <a href="https://github.com/leiju91/nexten-test" target="_blank">Liens vers le code sur github.com</a>
             <h2>Importer une API contenant des pays !</h2>
             <Continents />            
             <Countries /> 
